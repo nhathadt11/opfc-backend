95dbd64fece7e6f075257346913891654c41fa15
Dong Luong <donglv1995@gmail.com>
2018-10-02 23:34:31 +0700

    - added api create event planner

---
+++ OPFC.API.Services/Implementations/UserAPIService.cs
@@ -0,0 +1,29 @@
+﻿using AutoMapper;
+using OPFC.API.DTO;
+using OPFC.API.ServiceModel.User;
+using OPFC.API.Services.Interfaces;
+using OPFC.Models;
+using OPFC.Services.Interfaces;
+using ServiceStack;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OPFC.API.Services.Implementations
+{
+    public class UserAPIService : Service, IUserAPIService
+    {
+        private IUserService _userService = AppHostBase.Instance.Resolve<IUserService>();
+
+        public CreateEventPlannerResponse Post(CreateEventPlannerRequest request)
+        {
+            var user = Mapper.Map<UserDTO>(request.User);
+
+            var result = _userService.CreateUser(Mapper.Map<User>(user));
+            return new CreateEventPlannerResponse
+            {
+                User = Mapper.Map<UserDTO>(result)
+            };
+        }
+    }
+}
---
+++ OPFC.API.Services/Interfaces/IUserAPIService.cs
@@ -0,0 +1,12 @@
+﻿using OPFC.API.ServiceModel.User;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OPFC.API.Services.Interfaces
+{
+    public interface IUserAPIService
+    {
+        CreateEventPlannerResponse Post(CreateEventPlannerRequest request);
+    }
+}
